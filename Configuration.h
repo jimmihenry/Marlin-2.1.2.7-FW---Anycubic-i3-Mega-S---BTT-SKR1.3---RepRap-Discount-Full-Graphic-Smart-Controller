77678f9d3d0f38c3ded6079db1c9230897bb5b72
jimmihenry <89158909+jimmihenry@users.noreply.github.com>
2026-05-15 02:15:07 +0200

    Update Configuration.h
    Ready 2 USE Marlin v2.1.2.7 for Anycubic i3 Mega (S), BTT SKR1.3, RepRapp Discount Full Graphic Smart Controller, TMC2208 (Stand Alone).

--- Configuration.h
+++ Configuration.h
@@ -140,7 +140,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Analcubic i3 Mega SKR1.3"
+#define CUSTOM_MACHINE_NAME "Analcubic"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
